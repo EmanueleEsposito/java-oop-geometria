c6afa1ff9b426404e73fc81164fb130c46b65abc
EmanueleEsposito <manu2022@blu.it>
2022-06-14 16:50:15 +0200

    fine esercizio

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,29 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//Chiedere all0utente di inserire i valori di base e altezza
 
+		Scanner scan = new Scanner(System.in);
+		Rettangolo nuovoRettangolo = new Rettangolo();
+		int altezzaUtenteRettangolo = 0;
+
+		int baseUtenteRettangolo = 0;
+		System.out.print("Inserisci la base del rettangolo: ");
+		altezzaUtenteRettangolo = scan.nextInt();
+		System.out.print("Inserisci l'altezza del rettangolo: ");
+		baseUtenteRettangolo = scan.nextInt();
+		nuovoRettangolo.altezza = altezzaUtenteRettangolo;
+		nuovoRettangolo.base = baseUtenteRettangolo;
+//Stampare a schermo area e perimetro del rettangolo
+		System.out.println("L'area del rettangolo è: " + nuovoRettangolo.areaDelRettangolo());
+		System.out.println("Il perimetro del rettangolo è: " + nuovoRettangolo.perimetroDelRettangolo());
+
+		scan.close();
 	}
 
 }
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -1,5 +1,18 @@
 package jana60.geometria;
 
 public class Rettangolo {
+	// attributi
+	int base;
+	int altezza;
 
+	// metodo che calcola e restituisce l'area del rettangolo
+	int areaDelRettangolo() {
+		return base * altezza;
+
+	}
+
+	// metodo che calcola e restituisce il perimetro del rettangolo
+	int perimetroDelRettangolo() {
+		return (base + altezza) * 2;
+	}
 }
